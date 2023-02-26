54f8389c7a7e885886d9afb4622c3ca2b77f575e
manmeet <manmeetmoudgill76@gmail.com>
2023-02-26 17:26:06 +0100

    files modified

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,10 @@
-import React, { memo } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { memo, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { useApi } from "../shell/hooks/custom-http";
-import { VideosResponse } from "../models/video";
+import { Video, VideosResponse } from "../models/video";
 import Card from "../components/Card";
-import { Box, Button, Typography } from "@mui/material";
+import { NotFound } from "../components/NotFound";
 
 const Container = styled.div`
   display: flex;
@@ -15,30 +15,25 @@ const Container = styled.div`
 const Search = () => {
   const location = useLocation();
 
-  const { result: data } = useApi<VideosResponse>({
+  const { makeCall: getVideosBySearch } = useApi<VideosResponse>({
     url: `/videos/search${location?.search}`,
     method: "get",
-    onBootstrap: true,
+    onBootstrap: false,
   });
 
-  const navigate = useNavigate();
+  const [data, setData] = useState<Video[] | undefined>(undefined);
+  useEffect(() => {
+    getVideosBySearch().then((res) => {
+      if (res?.status === 200 || res?.status === 201) {
+        setData(res?.videos);
+      }
+    });
+  }, [location?.search, getVideosBySearch]);
+
   return (
     <Container>
-      {data?.videos?.length === 0 && (
-        <>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="h6">No videos found</Typography>
-            <Button onClick={() => navigate("/")}>Return to home</Button>
-          </Box>
-        </>
-      )}
-      {data?.videos?.map((video) => {
+      {data?.length === 0 && <NotFound />}
+      {data?.map((video) => {
         return <Card key={video?._id} video={video} />;
       })}
     </Container>
