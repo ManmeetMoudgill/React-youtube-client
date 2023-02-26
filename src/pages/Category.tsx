6337033dba9b136551e7fc38a748d63f80039cb9
manmeet <manmeetmoudgill76@gmail.com>
2023-02-26 16:51:02 +0100

    files modified on dev

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,8 +1,8 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { useApi } from "../shell/hooks/custom-http";
-import { VideosResponse } from "../models/video";
+import { Video, VideosResponse } from "../models/video";
 import Card from "../components/Card";
 import { NotFound } from "../components/NotFound";
 const Container = styled.div`
@@ -11,19 +11,31 @@ const Container = styled.div`
 
 const Category = () => {
   const params = useParams();
-  console.log(params);
-  const { result: videos, isLoading } = useApi<VideosResponse>({
-    url: `/videos/tags/?tags=${params}`,
-    method: "get",
-    onBootstrap: true,
-  });
+  const { isLoading, makeCall: getVideoBasedOnCategory } =
+    useApi<VideosResponse>({
+      url: `/videos/tags/?tags=${params?.category}`,
+      method: "get",
+      onBootstrap: false,
+    });
+  const [data, setData] = useState<Video[] | undefined>(undefined);
+
+  useEffect(() => {
+    if (!params?.category) return;
+    getVideoBasedOnCategory().then((res) => {
+      if (res?.status === 200 || res?.status === 201) {
+        setData(res?.videos);
+      }
+    });
+  }, [params?.category, getVideoBasedOnCategory]);
+
   return (
     <Container>
-      {videos?.videos &&
-        videos?.videos?.map((video) => {
+      {!isLoading &&
+        data &&
+        data?.map((video) => {
           return <Card key={video?._id} video={video} />;
         })}
-      {!isLoading && videos?.videos?.length === 0 && <NotFound />}
+      {!isLoading && data?.length === 0 && <NotFound />}
     </Container>
   );
 };
