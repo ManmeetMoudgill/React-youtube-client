f7b695983ff1c28591b5ec9e2d9fd95c8153be3f
Manmeet Moudgill <75222146+ManmeetMoudgill@users.noreply.github.com>
2023-06-04 12:45:47 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-console.log("hello there");
+
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
